5159ebb5d8fab41e73816c078e68a92eb3790c1f
Willy-Akampurira <willyakampurira741@gmail.com>
2026-05-31 19:15:47 +0300

    Updated vite run builds

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,17 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        chunkSizeWarningLimit: 1000, // Raises the warning threshold safely to 1MB
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        // Splits third-party packages into a separate vendor file for better loading performance
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+    },
 });
